cf749b4fb65b06fb0e550fea066a10bcacf0bf76
Morteza Rahimi <44939554+Mozart4242@users.noreply.github.com>
2022-10-03 23:18:08 +0330

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-import time
-from progressbar import progressbar
-from stringcolor import *
-
-# x == 0 -> success
-# x == 256 -> no output
-# x == 32512 -> error command
-# os.waitstatus_to_exitcode(x)
